355aee7023fe7ea7dc4bef4ba7246296ba753cdd
Alexandre Santiago <alexandresantiagobs@gmail.com>
2019-07-27 00:48:32 -0300

    Add project files.

---
+++ Cadastro_Projeto_BD/CadastroPessoa.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Cadastro_Projeto_BD
+{
+    partial class frmPessoa
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPessoa));
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.mtxtCpf = new System.Windows.Forms.MaskedTextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.mtxtData = new System.Windows.Forms.MaskedTextBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblValidaCpf = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrgEmis = new System.Windows.Forms.TextBox();
+            this.lblUF = new System.Windows.Forms.Label();
+            this.cmbUf = new System.Windows.Forms.ComboBox();
+            this.lblRg = new System.Windows.Forms.Label();
+            this.mtxtRg = new System.Windows.Forms.MaskedTextBox();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.bntConcluir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(10, 42);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(352, 26);
+            this.txtNome.TabIndex = 0;
+            // 
+            // mtxtCpf
+            // 
+            this.mtxtCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtxtCpf.Location = new System.Drawing.Point(386, 42);
+            this.mtxtCpf.Mask = "000,000,000-00";
+            this.mtxtCpf.Name = "mtxtCpf";
+            this.mtxtCpf.Size = new System.Drawing.Size(126, 26);
+            this.mtxtCpf.TabIndex = 1;
+            this.mtxtCpf.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mtxtCpf.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.mtxtCpf.UseWaitCursor = true;
+            this.mtxtCpf.TextChanged += new System.EventHandler(this.MtxtCpf_TextChanged);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(7, 26);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(383, 26);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(27, 13);
+            this.lblCpf.TabIndex = 3;
+            this.lblCpf.Text = "CPF";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(10, 43);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(352, 26);
+            this.txtEmail.TabIndex = 4;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // mtxtData
+            // 
+            this.mtxtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtxtData.Location = new System.Drawing.Point(10, 101);
+            this.mtxtData.Mask = "00/00/0000";
+            this.mtxtData.Name = "mtxtData";
+            this.mtxtData.Size = new System.Drawing.Size(99, 26);
+            this.mtxtData.TabIndex = 5;
+            this.mtxtData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mtxtData.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            this.mtxtData.ValidatingType = typeof(System.DateTime);
+            this.mtxtData.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.MtxtData_MaskInputRejected);
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(7, 85);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(102, 13);
+            this.lblData.TabIndex = 6;
+            this.lblData.Text = "Data de nascimento";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblValidaCpf);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtOrgEmis);
+            this.groupBox1.Controls.Add(this.lblUF);
+            this.groupBox1.Controls.Add(this.cmbUf);
+            this.groupBox1.Controls.Add(this.lblRg);
+            this.groupBox1.Controls.Add(this.mtxtRg);
+            this.groupBox1.Controls.Add(this.lblSexo);
+            this.groupBox1.Controls.Add(this.cmbSexo);
+            this.groupBox1.Controls.Add(this.lblData);
+            this.groupBox1.Controls.Add(this.mtxtData);
+            this.groupBox1.Controls.Add(this.lblCpf);
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Controls.Add(this.mtxtCpf);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(523, 150);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações pessoais";
+            // 
+            // lblValidaCpf
+            // 
+            this.lblValidaCpf.AutoSize = true;
+            this.lblValidaCpf.Location = new System.Drawing.Point(416, 26);
+            this.lblValidaCpf.Name = "lblValidaCpf";
+            this.lblValidaCpf.Size = new System.Drawing.Size(0, 13);
+            this.lblValidaCpf.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Órgão emissor";
+            // 
+            // txtOrgEmis
+            // 
+            this.txtOrgEmis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOrgEmis.Location = new System.Drawing.Point(417, 101);
+            this.txtOrgEmis.Name = "txtOrgEmis";
+            this.txtOrgEmis.Size = new System.Drawing.Size(95, 26);
+            this.txtOrgEmis.TabIndex = 13;
+            // 
+            // lblUF
+            // 
+            this.lblUF.AutoSize = true;
+            this.lblUF.Location = new System.Drawing.Point(331, 85);
+            this.lblUF.Name = "lblUF";
+            this.lblUF.Size = new System.Drawing.Size(21, 13);
+            this.lblUF.TabIndex = 12;
+            this.lblUF.Text = "UF";
+            // 
+            // cmbUf
+            // 
+            this.cmbUf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbUf.FormattingEnabled = true;
+            this.cmbUf.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AM",
+            "AP",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MG",
+            "MS",
+            "MT",
+            "PA",
+            "PB",
+            "PE",
+            "PI",
+            "PR",
+            "RJ",
+            "RN",
+            "RO",
+            "RR",
+            "RS",
+            "SC",
+            "SE",
+            "SP",
+            "TO"});
+            this.cmbUf.Location = new System.Drawing.Point(334, 101);
+            this.cmbUf.Name = "cmbUf";
+            this.cmbUf.Size = new System.Drawing.Size(51, 28);
+            this.cmbUf.TabIndex = 11;
+            // 
+            // lblRg
+            // 
+            this.lblRg.AutoSize = true;
+            this.lblRg.Location = new System.Drawing.Point(222, 85);
+            this.lblRg.Name = "lblRg";
+            this.lblRg.Size = new System.Drawing.Size(23, 13);
+            this.lblRg.TabIndex = 10;
+            this.lblRg.Text = "RG";
+            // 
+            // mtxtRg
+            // 
+            this.mtxtRg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtxtRg.Location = new System.Drawing.Point(225, 101);
+            this.mtxtRg.Mask = "00,000,000";
+            this.mtxtRg.Name = "mtxtRg";
+            this.mtxtRg.Size = new System.Drawing.Size(89, 26);
+            this.mtxtRg.TabIndex = 9;
+            this.mtxtRg.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(135, 85);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 8;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cmbSexo.Location = new System.Drawing.Point(138, 101);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(58, 28);
+            this.cmbSexo.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblEmail);
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox2.Location = new System.Drawing.Point(15, 168);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(523, 88);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Contato";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(7, 27);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // bntConcluir
+            // 
+            this.bntConcluir.Enabled = false;
+            this.bntConcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntConcluir.Location = new System.Drawing.Point(442, 262);
+            this.bntConcluir.Name = "bntConcluir";
+            this.bntConcluir.Size = new System.Drawing.Size(96, 30);
+            this.bntConcluir.TabIndex = 9;
+            this.bntConcluir.Text = "Concluir";
+            this.bntConcluir.UseVisualStyleBackColor = true;
+            this.bntConcluir.Click += new System.EventHandler(this.bntConcluir_Click);
+            // 
+            // frmPessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 312);
+            this.Controls.Add(this.bntConcluir);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPessoa";
+            this.Text = "Cadastro de cliente/funcionario";
+            this.Load += new System.EventHandler(this.FrmPessoa_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.MaskedTextBox mtxtCpf;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.MaskedTextBox mtxtData;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.ComboBox cmbUf;
+        private System.Windows.Forms.Label lblRg;
+        private System.Windows.Forms.MaskedTextBox mtxtRg;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrgEmis;
+        private System.Windows.Forms.Label lblUF;
+        private System.Windows.Forms.Button bntConcluir;
+        private System.Windows.Forms.Label lblValidaCpf;
+    }
+}
+
---
+++ Cadastro_Projeto_BD/CadastroPessoa.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Cadastro_Projeto_BD {
+    public partial class frmPessoa : Form {
+
+
+        public frmPessoa() {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+
+        private void bntConcluir_Click(object sender, EventArgs e) {
+
+            DateTime data = DateTime.Parse(mtxtData.Text);
+
+            string connectionString = @"Data Source=AS-NOTEBOOK\AS_BD;Initial Catalog=agile_market;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string sql = "INSERT INTO Pessoa (nome, cpf, dt_nasc,sexo,email,rg_numero,rg_uf,rg_org_emi) " +
+                         "VALUES (@nome, @cpf, @dt_nasc,@sexo,@email,@rg_numero,@rg_uf,@rg_org_emi)";
+
+            try {
+                SqlCommand C = new SqlCommand(sql, conn);
+
+                C.Parameters.Add(new SqlParameter("@nome", this.txtNome.Text));
+                C.Parameters.Add(new SqlParameter("@cpf", this.mtxtCpf.Text));
+                C.Parameters.Add(new SqlParameter("@dt_nasc", data));
+                C.Parameters.Add(new SqlParameter("@sexo", this.cmbSexo.Text));
+                C.Parameters.Add(new SqlParameter("@email", this.txtEmail.Text));
+                C.Parameters.Add(new SqlParameter("@rg_numero", this.mtxtRg.Text));
+                C.Parameters.Add(new SqlParameter("@rg_uf", this.cmbUf.Text));
+                C.Parameters.Add(new SqlParameter("@rg_org_emi", this.txtOrgEmis.Text));
+
+                conn.Open();
+
+                C.ExecuteNonQuery();
+
+                conn.Close();
+
+                MessageBox.Show("Cadastro realizado com sucesso!");
+
+                txtNome.Text = "";
+                mtxtCpf.Text = "";
+                mtxtData.Text = "";
+                cmbSexo.Text = "";
+                txtEmail.Text = "";
+                mtxtRg.Text = "";
+                cmbUf.Text = "";
+                txtOrgEmis.Text = "";
+                lblValidaCpf.Text = "";
+            }
+            catch (SqlException ex) {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+            catch (Exception ex) {
+                MessageBox.Show("ERRO: " + ex.Message);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) {
+        }
+        private void MtxtData_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) {
+        }
+
+        private void MtxtCpf_TextChanged(object sender, EventArgs e) {
+            if (mtxtCpf.Text.Length == 11) {
+                try {
+                    string cpf, cpfFinal, digitos;
+                    int dig1 = 0, dig2 = 0, soma = 0;
+
+                    cpf = mtxtCpf.Text;
+                    cpfFinal = cpf;
+                    cpf = cpfFinal.Substring(0, 9);
+
+                    for (int i = 0; i < 9; i++) {
+                        digitos = cpf.Substring(i, 1);
+                        soma = (int.Parse(digitos) * (10 - i));
+                        dig1 += soma;
+                    }
+                    dig1 = dig1 % 11;
+
+                    if (dig1 < 2) {
+                        dig1 = 0;
+                    }
+                    else {
+                        dig1 = 11 - dig1;
+                    }
+
+                    cpf += Convert.ToString(dig1);
+
+                    for (int i = 0; i < 10; i++) {
+                        digitos = (cpf.Substring(i, 1));
+                        soma = (int.Parse(digitos) * (11 - i));
+                        dig2 += soma;
+                    }
+                    dig2 = dig2 % 11;
+
+                    if (dig2 < 2) {
+                        dig2 = 0;
+                    }
+                    else {
+                        dig2 = 11 - dig2;
+                    }
+
+                    cpf += Convert.ToString(dig2);
+
+                    if (cpfFinal == cpf) {
+                        lblValidaCpf.ForeColor = Color.Green;
+                        lblValidaCpf.Text = "CPF Válido";
+                        bntConcluir.Enabled = true;
+                    }
+
+                    else {
+                        lblValidaCpf.ForeColor = Color.Red;
+                        lblValidaCpf.Text = "CPF Inválido ";
+                        bntConcluir.Enabled = false;
+                    }
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("ERRO: " + ex.Message);
+                    mtxtCpf.Text = "";
+                }
+            }
+        }
+
+        private void FrmPessoa_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
